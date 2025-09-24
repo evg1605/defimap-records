99f553f03bbc5212f5ef0e142bbd296b3ff619a2
evg1605 <evg1605@gmail.com>
2025-09-24 09:09:54 +0800

    fix v2

--- app/defaults.js
+++ app/defaults.js
@@ -1,5 +1,5 @@
 const defaultFromDate = '2025-06-08';
 const defaultFromTo = '2025-08-08';
 const defaultAccounts = ["0xeeA92D1E10768Cc89Dc79CB6bC8749dd17bB6D23", "0xbEEb6d5a07e4DC21C59E83d5c1124Ded4c03D452"];
-// const apiUrl = 'http://localhost:8090/api/gql/query';
-const apiUrl = 'https://defi-map.com/api/api/gql/query';
+const apiUrl = 'http://localhost:8090/api/gql/query';
+// const apiUrl = 'https://defi-map.com/api/api/gql/query';
